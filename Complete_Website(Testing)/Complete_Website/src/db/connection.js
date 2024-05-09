363fe3f9ac8fab28dea131410f2f39954d8a6d30
Surya <suryask7549@gmail.com>
2024-05-09 22:22:32 +0530

    Testing File Added

--- Complete_Website(Testing)/Complete_Website/src/db/connection.js
+++ Complete_Website(Testing)/Complete_Website/src/db/connection.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 
 (async function () {
   try {
-    // console.log(process.env.DB_NAME);
-    await mongoose.connect(`mongodb://localhost:27017/Complete_WebSite`);
+    await mongoose.connect(`mongodb://localhost:27017/Complete_WebSite(Testing)`);
     console.log("Connection SuccessFul");
   } catch (error) {
     console.log("Connection Failed", error);;
--- Complete_Website(Testing)/Complete_Website/src/index.js
+++ Complete_Website(Testing)/Complete_Website/src/index.js
@@ -18,6 +18,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', view_Path);
 app.set("view engine", "hbs");
 hbs.registerPartials(partial_Path);
+
+
+
+
 app.get('/index', (req, res) => {
   res.render('index');
 })
@@ -25,8 +29,7 @@ app.get('/login', (req, res) => {
   res.render('login');
 })
 
-app.get("/gallery", auth, (req, res) => {
-  // console.log(req.cookies.jwt, " From Cookies Browser");
+app.get("/gallery", (req, res) => {
   res.render("gallery");
 })
 
@@ -36,30 +39,13 @@ app.post('/login', async (req, res) => {
     const password = req.body.user_password;
     const isUser = await UserCollection.findOne({ email: Email });
     console.log(isUser);
-    // console.log(`Acutal Password hashed  value : ${isUser.password}`);
-
-    // console.log(isUser.password);
     const isMatch = await bcrypt.compare(password, isUser.password);
     console.log(isMatch);
-
-    const token = await isUser.generateAuthToken();
-    // console.log("token is : ", token);
-    // console.log(token, " at login time");
-    // Cookies
-    // console.log("token from browser cookie ", req.cookies.jwt);
-    res.cookie("jwt", token, {
-      expires: new Date(Date.now() + 14000), // Expires in 14 second
-      httpOnly: true, // Cookie cannot be accessed by client-side JavaScript
-      // secure: true, // Cookie will only be sent over HTTPS
-      // sameSite: 'strict'
-    })
-    // console.log("assigned");
     if (isMatch) {
       res.status(200).render("logged_In_Successfully");
     }
     else {
       res.status(401).render("incorrect_Password");
-      // console.log(error);
     }
   } catch (error) {
     console.log(error);
@@ -71,8 +57,6 @@ app.get('/signup', (req, res) => {
 })
 
 app.post('/signup', async (req, res) => {
-  // res.render('signup');
-  // console.log(req.body.name);
   try {
     if (req.body.password === req.body.cf_password) {
       const userDocument = new UserCollection({
@@ -81,22 +65,6 @@ app.post('/signup', async (req, res) => {
         email: req.body.email,
         password: req.body.password
       })
-
-
-      const token = await userDocument.generateAuthToken();
-      // console.log("token is : ", token);
-
-      // Cookies
-
-      // res.cookie("jwt", token, {
-      //   expires: new Date(Date.now() + 60 * 60), // Expires in 1 hour
-      //   httpOnly: true, // Cookie cannot be accessed by client-side JavaScript
-      //   secure: true, // Cookie will only be sent over HTTPS
-      //   sameSite: 'strict' // Cookie will only be sent for same-site requests
-      // });
-      // console.log(cookie, "  Sent Successfully");
-      // console.log(token, " at Sign Up Time");
-
       await userDocument.save().then(() => {
         res.status(201).render("registered_Successfullly")
       }).catch((err) => {
@@ -112,15 +80,6 @@ app.post('/signup', async (req, res) => {
     console.log(error);
   }
 })
-
-app.get("/logout", auth, async (req, res) => {
-  res.clearCookie("jwt");
-  await req.user.save();
-  console.log("Logged out");
-  res.render("login");
-})
-
-
 app.listen(8000, () => {
   console.log("Listening at port ", 8000);
 })
