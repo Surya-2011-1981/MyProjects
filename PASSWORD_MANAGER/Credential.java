a9e6c474f90f339d8241b5c7de378acd7043be5d
Surya <suryask7549@gmail.com>
2025-05-28 19:09:32 +0530

    Password Manager Folder Updated

---
+++ PASSWORD_MANAGER/Credential.java
@@ -0,0 +1,23 @@
+public class Credential {
+    private String website;
+    private String username;
+    private String password;
+
+    public Credential(String website, String username, String password) {
+        this.website = website;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
